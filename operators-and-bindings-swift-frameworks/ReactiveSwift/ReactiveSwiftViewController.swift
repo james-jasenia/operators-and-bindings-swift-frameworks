7963ff4385eca556ee3ed3eccbea6902516617bb
James <james.jasenia@gmail.com>
2022-11-20 18:30:57 +1100

    Added Reactive Swift printToConsole operator.

--- operators-and-bindings-swift-frameworks/ReactiveSwift/ReactiveSwiftViewController.swift
+++ operators-and-bindings-swift-frameworks/ReactiveSwift/ReactiveSwiftViewController.swift
@@ -9,13 +9,30 @@ import UIKit
 import ReactiveSwift
 
 class ReactiveSwiftViewController: UIViewController {
+    
+    let disposables = CompositeDisposable()
+    let publisher = MutableProperty<String>("")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
         setupBindings()
+        publisher.swap("Reactive Swift")
     }
     
     private func setupBindings() {
-        
+        disposables += publisher.signal
+            .printToConsole()
+            .observeValues {
+                print($0 + " - from observeValues")
+            }
+    }
+}
+
+extension Signal where Value == String, Error == Never {
+    func printToConsole() -> Signal<String, Never> {
+        on(value: {
+            print($0 + " - \(#function)")
+        })
     }
 }
