d66fa3373b361154474caf05823ceca3dfacae75
James <james.jasenia@gmail.com>
2022-11-21 20:15:35 +1100

    Created `CombineViewModel` and updated `CombineViewController` responsibilities.

--- operators-and-bindings-swift-frameworks/Combine/CombineViewController.swift
+++ operators-and-bindings-swift-frameworks/Combine/CombineViewController.swift
@@ -10,33 +10,27 @@ import Combine
 
 class CombineViewController: CommonViewController {
 
+    let viewModel = CombineViewModel()
     var cancellable = Set<AnyCancellable>()
-    let buttonTitleSubject = PassthroughSubject<String, Never>()
-    let isLoadingSubject = CurrentValueSubject<Bool, Never>(true)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         addView(styledButton)
         setupBindings()
-        buttonTitleSubject.send("Combine")
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: { [weak self] in
-            self?.isLoadingSubject.send(false)
-        })
-
     }
     
     private func setupBindings() {
-        buttonTitleSubject
+        viewModel.buttonTitleSubject
             .printToConsole()
             .sink(receiveValue: {
                 print("\($0) - from sink")
             }).store(in: &cancellable)
         
-        buttonTitleSubject
+        viewModel.buttonTitleSubject
             .assign(to: \.normalTitle, on: styledButton)
             .store(in: &cancellable)
         
-        isLoadingSubject
+        viewModel.isLoadingSubject
             .assign(to: \.isLoading, on: styledButton)
             .store(in: &cancellable)
     }
---
+++ operators-and-bindings-swift-frameworks/Combine/CombineViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  CombineViewModel.swift
+//  operators-and-bindings-swift-frameworks
+//
+//  Created by james.jasenia on 21/11/2022.
+//
+
+import Combine
+import Foundation
+
+class CombineViewModel {
+    
+    private let mutableButtonTitleSubject = CurrentValueSubject<String, Never>("Combine")
+    var buttonTitleSubject: AnyPublisher<String, Never> {
+        mutableButtonTitleSubject.eraseToAnyPublisher()
+    }
+    
+    private let mutableIsLoadingSubject = CurrentValueSubject<Bool, Never>(true)
+    var isLoadingSubject: AnyPublisher<Bool, Never> {
+        mutableIsLoadingSubject.eraseToAnyPublisher()
+    }
+    
+    init() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: { [weak self] in
+            self?.mutableIsLoadingSubject.send(false)
+        })
+    }
+}
